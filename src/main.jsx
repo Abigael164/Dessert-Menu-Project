26c41bbce22f91c5d0c90eb62cf025dddd52d102
Abigael Jeptoo <your-github-jeptooabigael164@gmail.com>
2026-01-19 13:27:58 -0800

    Initial Dessert project setup

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
+import './index.css'
+//import Routing from './Routing.jsx';
+//import Routed from './Routed.jsx';
+//import Routered from './Routered.jsx';
+import RoutedDessert from './RouteDessert.jsx';
+
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <RoutedDessert />
+  </BrowserRouter>
+);
